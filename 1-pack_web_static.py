3c450f216520d276ae8c2612d73e3767c5555545
Kamanda <erickckamanda@gmail.com>
2024-07-13 16:22:33 -0700

    Task 1 Revised

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-'''This script compresses/zips the directory /data/web_static/'''
-from fabric.api import local, task
-from datetime import datetime
+from fabric.api import local
+from time import strftime
+from datetime import date
 
 
-@task
 def do_pack():
-    # Create the timestamp string
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    """ A script that generates archive the contents of web_static folder"""
 
-    # Ensure the versions directory exists
-    local("mkdir -p versions")
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
-    # Define the full path for the directory to compress
-    full_path = "web_static"
+        return "versions/web_static_{}.tgz".format(filename)
 
-    # Create the tarball
-    tarball = f"versions/web_static_{timestamp}.tgz"
-    result = local(f"tar -czvf {tarball} {full_path}")
-
-    if result.succeeded:
-        return tarball
-    else:
+    except Exception as e:
         return None
